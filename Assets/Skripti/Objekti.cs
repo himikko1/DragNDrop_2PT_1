f90c20fa7a0d5727e1ff0ec2ddfdd84a54a09fd8
Sofija Dišlovaja <sofija.dyshlovaya@gmail.com>
2023-05-25 12:08:28 +0300

    Papildinaju kodu

--- Assets/Skripti/Objekti.cs
+++ Assets/Skripti/Objekti.cs
@@ -6,6 +6,14 @@ public class Objekti : MonoBehaviour {
 	public GameObject atkritumuMasina;
 	public GameObject atraPalidziba;
 	public GameObject autobuss;
+	public GameObject policija;
+	public GameObject ugunsdzeseji;
+	public GameObject e46;
+	public GameObject e61;
+	public GameObject b2;
+	public GameObject traktors5;
+	public GameObject cementaMasina;
+	public GameObject eskavators;
 
 	[HideInInspector]
 	public Vector2 atkrMKoord;
@@ -13,8 +21,25 @@ public class Objekti : MonoBehaviour {
 	public Vector2 atrPKoord;
 	[HideInInspector]
 	public Vector2 bussKoord;
+    [HideInInspector]
+    public Vector2 policijaKoord;
+    [HideInInspector]
+    public Vector2 ugunsdzesejiKoord;
+    [HideInInspector]
+    public Vector2 e46Koord;
+    [HideInInspector]
+    public Vector2 eskavatorsKoord;
+    [HideInInspector]
+    public Vector2 cementaMasinaKoord;
+    [HideInInspector]
+    public Vector2 traktors5Koord;
+    [HideInInspector]
+    public Vector2 e61Koord;
+    [HideInInspector]
+    public Vector2 b2Koord;
 
-	public Canvas kanva;
+
+    public Canvas kanva;
 
 	public AudioSource skanasAvots;
 	public AudioClip[] skanasKoAtskanot;
@@ -34,5 +59,29 @@ public class Objekti : MonoBehaviour {
 
         bussKoord =
 		autobuss.GetComponent<RectTransform>().localPosition;
+
+        policijaKoord =
+        policija.GetComponent<RectTransform>().localPosition;
+
+        ugunsdzesejiKoord =
+        ugunsdzeseji.GetComponent<RectTransform>().localPosition;
+
+        e46Koord =
+        e46.GetComponent<RectTransform>().localPosition;
+
+        eskavatorsKoord =
+      eskavators.GetComponent<RectTransform>().localPosition;
+
+        b2Koord =
+     b2.GetComponent<RectTransform>().localPosition;
+
+        traktors5Koord =
+     traktors5.GetComponent<RectTransform>().localPosition;
+
+        e61Koord =
+     e61.GetComponent<RectTransform>().localPosition;
+
+        cementaMasinaKoord =
+     cementaMasina.GetComponent<RectTransform>().localPosition;
     }
 }
